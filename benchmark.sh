a0cb5d1262316e14cd6f4cfedb3a7262f7300263
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2018-10-22 09:51:48 -0400

    typos

--- benchmark.sh
+++ benchmark.sh
@@ -6,8 +6,8 @@ ARGS="--gpu --data $DATA_LOCATION --static-loss-scale 128 --prof 10"
 BATCH_SIZE="128 256"
 WORKERS="1 2 4 8 16"
 
-for batch in BATCH_SIZE do
-    for worker in WORKERS do
+for batch in $BATCH_SIZE; do
+    for worker in $WORKERS; do
         resnet-18-pt $ARGS -b $batch -j $worker
         resnet-18-pt $ARGS -b $batch -j $worker --half
 
