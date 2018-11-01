6a30633f882a330979ef63e2ac2420d12046fc14
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2018-11-01 15:35:42 -0400

    Dali Image File DB is created in the temp folder

--- MixedPrecision/tools/folder.py
+++ MixedPrecision/tools/folder.py
@@ -3,6 +3,7 @@ import os
 import fnmatch
 import time
 import hashlib
+import tempfile
 
 
 def pytorch_folder_visitor(folder):
@@ -30,20 +31,26 @@ def dali_folder_visitor(folder) -> List[str]:
 
 
 def make_dali_cached_file_list_which_is_also_a_file(data_dir):
-        start = time.time()
-        h = hashlib.sha256()
-        h.update(data_dir.encode('utf-8'))
-        file_name = 'tmp_' + h.hexdigest()
+    # I want to make a temp file that is going to be persistent for `some` time
+    # so the file name should be consistent and unique per folder
 
-        if not os.path.isfile(file_name):
-            images = '\n'.join(dali_folder_visitor(data_dir))
-            file = open(file_name, 'w')
-            file.write(images)
-            file.close()
+    h = hashlib.sha256()
+    h.update(data_dir.encode('utf-8'))
+    temp_dir = tempfile.gettempdir()
+    file_name = temp_dir + '/tmp_image_db_dali_' + h.hexdigest()
+
+    if not os.path.isfile(file_name):
+        start = time.time()
+
+        images = '\n'.join(dali_folder_visitor(data_dir))
+        file = open(file_name, 'w')
+        file.write(images)
+        file.close()
 
         end = time.time()
         print('Took {} to walk folder'.format(end - start))
-        return file_name
+
+    return file_name
 
 
 if __name__ == '__main__':
