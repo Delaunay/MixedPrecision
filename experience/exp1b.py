fe1c2d9fe0a6073abb87589995a06c49244c9071
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2019-01-30 09:06:44 -0600

    -

--- experience/exp1b.py
+++ experience/exp1b.py
@@ -21,18 +21,29 @@ class HybridLoss:
         self.mode = mode
         self.classifier_loss = 0
         self.regression_loss = 0
+        self.to_image = transforms.ToPILImage()
+        self.resize = transforms.Resize((32, 32))
+        self.gray = transforms.Grayscale()
+        self.to_tensor = transforms.ToTensor()
 
     def __call__(self, y, y_hat, x, x_hat):
         # x_hat has only one channel
 
-        with torch.no_grad():
-            x_gray = x.mean(dim=1)
-
         # return self.mode * self.classifier_loss(y_hat, y) + (1 - self.mode) * ((x_hat - x_gray) ** 2)
-        self.classifier_loss = self.classifier_loss_function(y_hat, y)
-        self.regression_loss = self.regression_loss_function(x_hat, x_gray) ** 0.25  #((x_hat - x_gray) ** 2).mean()
+        self.classifier_loss = self.mode * self.classifier_loss_function(y_hat, y)
 
-        v = self.classifier_loss * self.regression_loss
+        # This is awful but since we upsample CIFAR10 and that torch does not give us access to the original img...
+        # this greatly help training since we only reconstruct a 32x32 img and not a 224x224!!
+        x = x.cpu().view(-1, 3, 224, 224)
+        xgpu = torch.tensor((), dtype=torch.float)
+        xgpu = xgpu.new_zeros((len(x), 3, 32, 32)).cuda()
+
+        for i, img in enumerate(x):
+            xgpu[i] = self.to_tensor(self.resize(self.to_image(img + 0.5))).cuda()
+
+        self.regression_loss = (1 - self.mode) * self.regression_loss_function(x_hat, xgpu)  #((x_hat - x_gray) ** 2).mean()
+
+        v = self.classifier_loss + self.regression_loss
 
         self.classifier_loss = self.classifier_loss.item()
         self.regression_loss = self.regression_loss.item()
@@ -45,7 +56,7 @@ class HybridLoss:
 
 class HybridClassifier(nn.Module):
 
-    def __init__(self, classes=1000, output=(224, 224)):
+    def __init__(self, classes=1000, output=(32, 32)):
         super(HybridClassifier, self).__init__()
         # self.resizer = nn.Linear(3 * 32 * 32, 3 * 224 * 224)
         # torch.nn.ConvTranspose2d()
@@ -53,18 +64,33 @@ class HybridClassifier(nn.Module):
         self.rectify_fc2_backward = []
         self.classify_fc_backward = []
 
-        self.resnet = TruncatedResNet(resnet.BasicBlock, [2, 2, 2, 2])
+        resnet18 = [2, 2, 2, 2]
+        resnet50 = [3, 4, 6, 3]
+
+        self.resnet = TruncatedResNet(resnet.BasicBlock, resnet50)
         self.classify_fc = nn.Linear(512 * resnet.BasicBlock.expansion, classes)
-        self.classify_fc.register_backward_hook(lambda x: self.classify_fc_backward.append(x.mean().item()))
+        # self.classify_fc.register_backward_hook(self.classify_callback)
 
         # torch.Size([64, 512, 7, 7]) => torch.Size([64, 8, 15, 15])
         self.rectify_fc1 = nn.ConvTranspose2d(512, 8, kernel_size=3, stride=2)
         # torch.Size([64, 8, 15, 15]) => 64, 1, 224, 224
-        self.rectify_fc2 = nn.Linear(8 * 15 * 15, output[0] * output[1])
-        self.rectify_fc2.register_backward_hook(lambda x: self.rectify_fc2_backward.append(x.mean().item()))
+        self.rectify_fc2 = nn.Linear(8 * 15 * 15, 3 * output[0] * output[1])
+        # self.rectify_fc2.register_backward_hook(self.rectify_callback)
 
         self.output = output
         self.name = 'Experience1b'
+
+    def rectify_callback(self, module, input, output):
+        elem = [None] * len(output)
+        for i, val in enumerate(output):
+            elem[i] = val.mean().item()
+        self.rectify_fc2_backward = elem
+
+    def classify_callback(self, module, input, output):
+        elem = [None] * len(output)
+        for i, val in enumerate(output):
+            elem[i] = val.mean().item()
+        self.classify_fc_backward = elem
 
     def forward(self, x):
         # x = self.resizer(x.view(-1, 3, 32, 32))
@@ -79,7 +105,7 @@ class HybridClassifier(nn.Module):
         # x_hat = self.rectify_fc2(x_hat)
         x_hat = self.rectify_fc2(x_hat.view(-1, 8 * 15 * 15))
 
-        return y_hat, x_hat.view(-1, self.output[0], self.output[1])
+        return y_hat, x_hat.view(-1, 3, self.output[0], self.output[1])
 
 
 class Experience1b(Baseline):
@@ -88,7 +114,7 @@ class Experience1b(Baseline):
         self.model.name = self.name = 'Experience1b'
 
     def after_batch(self, id, batch_context):
-        print('Rectify {} \nClassify {}'.format(self.model.rectify_fc2_backward, self.model.classify_fc_backward))
+        # print('Rectify {} \nClassify {}'.format(self.model.rectify_fc2_backward, self.model.classify_fc_backward))
         self.stats.write('{data[epoch][id]:2d}, {data[id]:4d}, {data[acc]:.2f}, {data[loss]:.4f}, {data[loss2]:.4f}\n'.format(data=batch_context))
         print('[{data[epoch][id]:2d} |{data[id]:4d}] acc: {data[acc]:.2f}%  loss: {data[loss]:.4f} {data[loss2]:.4f}'.format(data=batch_context))
 
@@ -172,11 +198,11 @@ class Experience1b(Baseline):
                     to_image = transforms.ToPILImage()
                     size = len(x_hat)
 
-                    x_hat = x_hat.cpu().view(size, 1, 224, 224)
+                    x_hat = x_hat.cpu().view(size, 3, 32, 32)
                     x = x.cpu().view(size, 3, 224, 224)
 
                     for i, img in enumerate(x_hat):
-                        hat = to_image(img + 0.5)
+                        hat = to_image(img + 0.5).resize((224, 224))
                         original = to_image(x[i] + 0.5)
 
                         img = self.merge_images(original, hat)
@@ -251,5 +277,25 @@ if __name__ == '__main__':
     #trainer.train()
     trainer.show_testset(args, False)
 
+"""
+    Resnet50 + Regression
+    
+    Train Set [49 | 772] acc: 95.31%  loss: 0.0131 0.0311
+    Train Set [49]       acc: 97.56%  loss: 0.0002
+    Test Set             acc: 98.44%  loss: 0.0365
 
+       Stage , Average , Deviation ,    Min ,    Max , count 
+ fetch_input ,  0.2176 ,    0.0475 , 0.0029 , 0.2527 , 39090 
+     forward ,  0.2006 ,    0.0061 , 0.0535 , 0.2453 , 39090 
+    backward ,  0.0136 ,    0.0024 , 0.0090 , 0.2809 , 39090 
+       Total ,  0.4319 ,    0.0477 , 0.0679 , 0.5259 , 39090 
+    
+             Metric ,   Average , Deviation ,       Min ,       Max , count 
+    temperature.gpu ,   67.7603 ,    0.9117 ,   63.0000 ,   70.0000 , 69770 
+    utilization.gpu ,   78.6346 ,   20.3697 ,   13.0000 ,  100.0000 , 69770 
+ utilization.memory ,   54.9997 ,   12.1643 ,    7.0000 ,   73.0000 , 69770 
+       memory.total , 6075.0000 ,    0.0000 , 6075.0000 , 6075.0000 , 69770 
+        memory.free , 1152.3804 ,   17.2144 ,  565.0000 , 3252.0000 , 69770 
+        memory.used , 4922.6196 ,   17.2144 , 2823.0000 , 5510.0000 , 69770
+"""
 
