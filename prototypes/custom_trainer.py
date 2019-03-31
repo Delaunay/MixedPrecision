35362d146ae8f1c6bf3809f220342524fda2cc4c
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2019-03-31 14:37:53 -0500

    RingBuffer

--- prototypes/custom_trainer.py
+++ prototypes/custom_trainer.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 
+import array
 import time
 import math
 
@@ -32,6 +33,29 @@ class OptimizerAdapter:
         return self.optimizer.load_state_dict(img)
 
 
+class Fp16OptimizerAdapter:
+    def __init__(self, optimizer: optim.Optimizer, **kwargs):
+        from apex import amp
+
+        self.optimizer = optimizer
+        self.amp = amp.init()
+
+    def zero_grad(self):
+        return self.optimizer.zero_grad()
+
+    def step(self, cost=None, closure=None, **kwargs):
+        return self.optimizer.step(closure)
+
+    def backward(self, cost: nn.Module):
+        with self.amp.scale_loss(cost, self.optimizer) as loss:
+            return loss.backward()
+
+    def state_dict(self):
+        return self.optimizer.state_dict()
+
+    def load_state_dict(self, img):
+        return self.optimizer.load_state_dict(img)
+
 
 class Trainer:
     def __init__(self):
@@ -42,6 +66,63 @@ class Trainer:
 
     def step(self, *args, **kwargs):
         pass
+
+"""
+b 	boolean
+i 	signed integer
+u 	unsigned integer
+f 	floating-point
+c 	complex floating-point
+m 	timedelta
+M 	datetime
+O 	object
+S 	(byte-)string
+U 	Unicode
+V 	void
+"""
+
+
+class RingBuffer:
+    types = {
+        torch.float16: 'f',  # 4
+        torch.float32: 'f',  # 4
+        torch.float64: 'd',  # 8
+
+        torch.int8:  'b',    # 1
+        torch.int16: 'h',    # 2
+        torch.int32: 'l',    # 4
+        torch.int64: 'q',    # 8
+
+        torch.uint8:  'B',   # 1
+        # torch.uint16: 'H',   # 2
+        # torch.uint32: 'L',   # 4
+        # torch.uint64: 'Q',   # 8
+    }
+
+    def __init__(self, size, dtype, default_val=0):
+        self.array = array.array(self.types[dtype], [default_val] * size)
+        self.capacity = size
+        self.offset = 0
+
+    def __getitem__(self, item):
+        return self.array[item % self.capacity]
+
+    def __setitem__(self, item, value):
+        self.array[item % self.capacity] = value
+
+    def append(self, item):
+        self.array[self.offset % self.capacity] = item
+        self.offset += 1
+
+    def to_list(self):
+        if self.offset < self.capacity:
+            return list(self.array[:self.offset])
+        else:
+            end_idx = self.offset % self.capacity
+            return list(self.array[end_idx: self.capacity]) + list(self.array[0:end_idx])
+
+    def __len__(self):
+        return min(self.capacity, self.offset)
 
 
 class Logger:
@@ -56,7 +137,8 @@ class Logger:
 
         self.total_batch_count = epoch_count * batch_count
 
-    def get_avg(self, stream: StatStream):
+    @staticmethod
+    def get_avg(stream: StatStream):
         avg = stream.avg
         if avg == 0:
             avg = stream.val
@@ -92,6 +174,9 @@ class Logger:
 
     def log_train(self, train_time, **kwargs):
         print(f'Trained in {train_time / 60:.2f} min')
+
+    def log_metric(self, metric, value):
+        pass
 
 
 class TrainClassifier(Trainer):
@@ -135,6 +220,8 @@ class TrainClassifier(Trainer):
 
             epoch_end = time.time()
             epoch_time.update(epoch_end - epoch_start)
+
+            self.logger.log_metric('loss', loss)
 
             # ---
             v = locals()
@@ -268,3 +355,15 @@ if __name__ == '__main__':
     sys.stderr = sys.stdout
 
     test()
+
+    #db = RoundRobinDatabase(10, torch.int32)
+
+    # for i in range(5):
+    #     db.append(i)
+    #
+    # print(db.to_list())
+    #
+    # for i in range(20):
+    #     db.append(i)
+    #
+    # print(db.to_list())
