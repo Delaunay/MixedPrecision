f396c0c7eea467826a192a16d931187f185e71ca
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2019-06-10 16:47:09 -0400

    update benzina dataloader

--- MixedPrecision/tools/benzina.py
+++ MixedPrecision/tools/benzina.py
@@ -5,13 +5,12 @@ def make_data_loader(args, size):
 
     dataset = benzina.torch.ImageNet(args.data)
 
-    return benzina.torch.NvdecodeDataLoader(
+    return benzina.torch.DataLoader(
         dataset,
         batch_size=args.batch_size,
         seed=args.seed,
         shape=(size, size),
-        warp_transform=None,
-        oob_transform=(0, 0, 0),
-        scale_transform=1 / 255,
+        warp_transform=benzina.torch.operations.SimilarityTransform(),
+        norm_transform=1 / 255,
         bias_transform=-0.5
     )
